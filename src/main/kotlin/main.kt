562ce3ee9e56fa053fd86c10b92a0801b094aa20
NJPM <nathanminter1992@gmail.com>
2020-09-23 12:50:12 +0100

    Loop examples

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,31 @@
 fun main() {
     println("Hello World!")
+
+    var a = 5                                               // Implies type
+    val b = 6                                               // Val(ue) is immutable
+    println("-- 1st for loop output --")                    // For loop examples
+    for (i in 10 downTo 1) {
+        println("i = $i")
+    }
+    println("\n-- 2nd for loop output --")
+    for (j in 1 until 10) {
+        println("j = $j")
+    }
+    println("\n-- 3rd for loop output --")
+    for (k in 1..10 step 2) {
+        println("k = $k")
+    }
+    val myTasks = setOf(                                    // Use mutableSetOf for a mutable set
+            "Eat Breakfast",                                // or listOf, mapOf, etc
+            "Clean Up",
+            "Work",
+            "Eat Lunch",
+            "Clean Up",
+            "Work",
+            "Eat Dinner",
+            "Clean Up")
+    println("\n-- myTasks Output --")                       // Use index in for loop
+    for ((taskIndex, task) in myTasks.withIndex()) {
+        println("$taskIndex: $task")
+    }
 }
